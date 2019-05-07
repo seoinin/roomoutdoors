247c724933c4f532b966d2e4fce5c7f61e7263e5
Nokdev1 <k00212688@student.lit.ie>
2019-05-07 20:05:04 +0100

    Added Scenes and Menu

---
+++ Assets/_Scripts/Scripts2/Menu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void Products()
+    {
+
+        SceneManager.LoadScene("Products");
+    }
+
+    public void RoomOutdoors2()
+    {
+
+        SceneManager.LoadScene("RoomOutdoors2");
+    }
+
+    public void Verandas()
+    {
+
+        SceneManager.LoadScene("Verandas");
+    }
+
+    public void GardenRooms()
+    {
+
+        SceneManager.LoadScene("GardenRooms");
+    }
+
+    public void GardenCabins()
+    {
+
+        SceneManager.LoadScene("GardenCabins");
+    }
+
+    public void Furniture()
+    {
+
+        SceneManager.LoadScene("Furniture");
+    }
+
+    public void Favourites()
+    {
+
+        SceneManager.LoadScene("Favourites");
+    }
+
+    public void Carports()
+    {
+
+        SceneManager.LoadScene("Carports");
+    }
+
+    public void Awnings()
+    {
+
+        SceneManager.LoadScene("Awnings");
+    }
+
+    public void QuitApp()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+
+
+    }
+}
